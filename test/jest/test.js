a9a262b7854c51a67bbdfa57cdb1923bd51ca64d
Jens Peters <jp7677@gmail.com>
2020-04-14 08:36:34 +0200

    Split test cases.

--- test/jest/test.js
+++ test/jest/test.js
@@ -1,8 +1,6 @@
 import { formatSelectors } from '../../src/lib'
 
 test.each([
-  ['p', 'p'],
-  ['div', 'div'],
   ['cy|my-custom-selector', '[data-cy="my-custom-selector"]'],
   ['cy|my-custom-selector + p', '[data-cy="my-custom-selector"] + p'],
   ['cy|my-custom-selector:disabled', '[data-cy="my-custom-selector"]:disabled'],
@@ -15,11 +13,17 @@ test.each([
     'body > .modal cy|my-custom-selector > .some-class cy|my-custom-selector-child cy|my-custom-selector-grand-child',
     'body > .modal [data-cy="my-custom-selector"] > .some-class [data-cy="my-custom-selector-child"] [data-cy="my-custom-selector-grand-child"]',
   ],
-  ['other-cy|my-custom-selector', 'other-cy|my-custom-selector'],
 ])(
   'should format the cy selector `%s` to proper data-cy selectors `%s`',
   (selectors, expectedResult) => {
     expect(formatSelectors(selectors)).toBe(expectedResult)
+  },
+)
+
+test.each([['p'], ['div'], ['other-cy|my-custom-selector']])(
+  'should not format the cy selector `%s` to data-cy selectors',
+  selectors => {
+    expect(formatSelectors(selectors)).toBe(selectors)
   },
 )
 
